347d4407ebb69cf0840c8ddc1ed95c17cd08eb29
Mai Pham <mai291294@gmail.com>
2023-02-25 21:50:05 -0500

    committed

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const path = require('path');
+const Campground = require('./models/campgound');
+
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', { //default port
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connextion error"));
+db.once("open", ()=>{
+    console.log("Database connected");
+})
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
