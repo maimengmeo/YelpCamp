472acba076f99fd864fb340eaae65d1a83ceb7a0
Mai Pham <mai291294@gmail.com>
2023-02-26 15:28:14 -0500

    committed

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
+const cities = require('./cities');
+const {places, descriptors} = require('./seedHelpers');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
-    useCreateIndex: true,
+    //useCreateIndex: true,
     useUnifiedTopology: true
 });
 
@@ -13,10 +15,17 @@ db.once('open', ()=>{
     console.log('Databse connected');
 });
 
+const sample = (array) => array[Math.floor(Math.random()*array.length)]; //to generate a random number then use this function to generate title
+
 const seedDB = async() => {
     await Campground.deleteMany({});
-    const c = new Campground({title: 'purple field'});
-    await c.save();
+    for(let i = 0; i < 50; i++) {
+        const rand = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[rand].city}, ${cities[rand].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`});
+        await camp.save();
+    }
 }
 
 seedDB();
